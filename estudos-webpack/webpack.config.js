11a07eaf0c369f023c2bff19b5dbe7cfd58c1d9b
Guilherme Souza <gui.edu0380@gmail.com>
2024-01-18 16:22:39 -0300

    build(webpack.config.js) alterando modo para produção

--- estudos-webpack/webpack.config.js
+++ estudos-webpack/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist')
     },
-    mode: 'development',
+    mode: 'production',
     module: {
         rules: [{
             test: /\.css$/,
